8ed95923f09dd5ee6f05d4a51afbf8466a544658
Denys Chernyshov <den.blackshov@gmail.com>
2024-01-05 16:24:09 +0100

    Add docker file

--- cups/setup.sh
+++ cups/setup.sh
@@ -37,3 +37,20 @@ lpoptions -d MFCL2710DW
 
 # Maybe just upload ppd file?
 # Maybe prebuild the image for rpi with driver already in.
+
+
+# install docker https://docs.docker.com/engine/install/raspberry-pi-os/
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up Docker's APT repository:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# https://blog.tericcabrel.com/springboot-github-actions-ci-cd/
